d3b70afef4c972adddf0ccf48514e5bdeb166f07
hungnqph19112 <nqh1703k2@gmail.com>
2024-12-24 01:02:33 +0700

    Villa CRUD

---
+++ WhiteLagoon.Application/DTOs/CreateVillaDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Application.DTOs
+{
+    public class CreateVillaDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty; 
+        [Display(Name = "Price per night")]
+        [Range(10, 10000)]
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        [Range(1, 10)]
+        public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
---
+++ WhiteLagoon.UI/Controllers/VillaController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Application.DTOs;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Context;
+
+namespace WhiteLagoon.UI.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateVillaDto item)
+        {
+            if (item.Name == item.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid) 
+            {
+                var newVilla = new Villa
+                {
+                    Name = item.Name,
+                    Description = item.Description,
+                    Price = item.Price,
+                    Sqft = item.Sqft,
+                    Occupancy = item.Occupancy,
+                    ImageUrl = item.ImageUrl,
+                };
+
+                _context.Villas.Add(newVilla);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        public IActionResult Update(Guid villaId)
+        {
+            var villa = _context.Villas.FirstOrDefault(v => v.Id == villaId);
+
+            if (villa == null)
+            {
+                return View("~/Views/Shared/NotFound.cshtml");
+            }
+
+            var item = new UpdateVillaDto
+            {
+                Id = villa.Id,
+                Name = villa.Name,
+                Description = villa.Description,
+                Price = villa.Price,
+                Sqft = villa.Sqft,
+                Occupancy = villa.Occupancy,
+                ImageUrl = villa.ImageUrl,
+            };
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Update(UpdateVillaDto item)
+        {
+            if (ModelState.IsValid)
+            {
+                var villa = _context.Villas.FirstOrDefault(v => v.Id == item.Id);
+
+                if (villa == null)
+                {
+                    return View("~/Views/Shared/NotFound.cshtml");
+                }
+
+                villa.Name = item.Name;
+                villa.Description = item.Description;
+                villa.Price = item.Price;
+                villa.Sqft = item.Sqft;
+                villa.Occupancy = item.Occupancy;
+                villa.ImageUrl = item.ImageUrl;
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public IActionResult Delete(Guid villaId)
+        {
+            var villa = _context.Villas.FirstOrDefault(v => v.Id == villaId);
+            if (villa == null)
+            {
+                return View("~/Views/Shared/NotFound.cshtml");
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa item)
+        {
+            var villa = _context.Villas.FirstOrDefault(v => v.Id == item.Id);
+            if (villa != null)
+            {
+                _context.Villas.Remove(villa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
