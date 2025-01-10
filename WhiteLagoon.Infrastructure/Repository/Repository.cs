165ccc03af9fb5f00f4a9cca17a120ca77d974b3
hungnqph19112 <nqh1703k2@gmail.com>
2025-01-10 08:41:08 +0700

    note Repository

--- WhiteLagoon.Infrastructure/Repository/Repository.cs
+++ WhiteLagoon.Infrastructure/Repository/Repository.cs
@@ -15,11 +15,16 @@ namespace WhiteLagoon.Infrastructure.Repository
     {
         private readonly ApplicationDbContext _context;
         internal DbSet<T> dbSet;
+        // DbSet<T> trong EF Core đại diện cho một tập hợp thực thể trong cơ sở dữ liệu.
+        // dbSet được sử dụng để truy cập dữ liệu từ một bảng tương ứng với kiểu thực thể T trong cơ sở dữ liệu.
+        // Sử dụng dbSet trong Repository được dùng để tóm gọn và trừu tượng hóa các thao tác với cơ sở dữ liệu. Thay vì làm việc trực tiếp với DbContext, bạn tương tác thông qua Repository.
 
         public Repository(ApplicationDbContext context)
         {
             _context = context;
             dbSet = _context.Set<T>();
+            // Khi gọi _context.Set<T>(), EF Core sẽ trả về một đối tượng DbSet<T> được ánh xạ tới bảng tương ứng trong cơ sở dữ liệu.
+            // Ví dụ: Nếu T là Villa, thì dbSet ánh xạ tới bảng Villas trong cơ sở dữ liệu.
         }
         public void Add(T entity)
         {
@@ -31,9 +36,17 @@ namespace WhiteLagoon.Infrastructure.Repository
             return dbSet.Any(filter);
         }
 
-        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet; // Thực thể được theo dõi
+            }
+            else
+            {
+                query = dbSet.AsNoTracking(); // Thực thể không được theo dõi (AsNoTracking trong EF Core được sử dụng để chỉ định rằng các thực thể trả về từ truy vấn sẽ không được theo dõi bởi DbContext)
+            }
 
             if (filter != null)
             {
@@ -52,9 +65,17 @@ namespace WhiteLagoon.Infrastructure.Repository
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null, bool tracked = false)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet; // Thực thể được theo dõi
+            }
+            else
+            {
+                query = dbSet.AsNoTracking(); // Thực thể không được theo dõi (AsNoTracking trong EF Core được sử dụng để chỉ định rằng các thực thể trả về từ truy vấn sẽ không được theo dõi bởi DbContext)
+            }
 
             if (filter != null)
             {
